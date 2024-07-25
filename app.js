6cee9883f166fe876a784f8d9da70208d90682c2
Homrts620 <hom620@gmail.com>
2024-07-25 19:15:27 +0200

    hw5

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-
-const contactsRouter = require('./routes/api/contacts');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+require("passport");
+require("./config/config-passport");
+const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use("/users", usersRouter);
+app.use("/api/contacts", contactsRouter);
+app.use("/avatars", express.static("public/avatars"));
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
---
+++ validate/userJoi.js
@@ -0,0 +1,40 @@
+const Joi = require("joi");
+
+const userJoi = Joi.object({
+    password: Joi.string().min(8).max(16).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+});
+
+const subscriptionJoi = Joi.object({
+    subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
+
+const userSubscriptionJoi = (req, res, next) => {
+const { error } = subscriptionJoi.validate(req.body);
+
+if (error) {
+    return res.status(400).json({
+        status: "error",
+        code: 400,
+        message: error.details[0].message,
+    });
+}
+next();
+};
+
+const userJoiValidate = (req, res, next) => {
+const result = Joi.validate(req.body, userJoi);
+
+if (result.error) {
+    const errorMessage = result.error.details
+    .map((detail) => detail.message)
+    .join(", ");
+    res.status(400).json({ message: errorMessage });
+} else {
+    next();
+}
+};
+module.exports = {
+    userJoiValidate,
+    userSubscriptionJoi,
+};
